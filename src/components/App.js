addec09dbca6db4b6d2ce00f0f923df0e326ddb8
Kiuzeo <56361197+Kiuzeo@users.noreply.github.com>
2021-03-05 18:31:15 +0100

    mobilefirst

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { get } from 'lodash'
-import Items from './Items'
-import SearchBar from './SearchBar'
-import Links from './Links'
-import { ModalContext, useModal } from '../contexts/modal'
-import FocusModal from './FocusModal'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { get } from "lodash";
+import Items from "./Items";
+import SearchBar from "./SearchBar";
+import Links from "./Links";
+import { ModalContext, useModal } from "../contexts/modal";
+import FocusModal from "./FocusModal";
 
 const App = () => {
-  const modal = useModal()
-  const [input, setInput] = useState('apollo')
-  const [collection, setCollection] = useState([])
-  const [loading, setLoading] = useState(false)
+  const modal = useModal();
+  const [input, setInput] = useState("apollo");
+  const [collection, setCollection] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const { items, links } = collection
+  const { items, links } = collection;
 
   const fetchNasaData = (url) => {
-    setLoading(true)
+    setLoading(true);
     axios.get(url).then((res) => {
-      setCollection(get(res, 'data.collection', null))
-      setLoading(false)
-    })
-  }
+      setCollection(get(res, "data.collection", null));
+      setLoading(false);
+    });
+  };
 
   useEffect(() => {
-    fetchNasaData(`https://images-api.nasa.gov/search?q=apollo&page=1`)
-  }, [])
+    fetchNasaData(`https://images-api.nasa.gov/search?q=apollo&page=1`);
+  }, []);
 
   return (
     <ModalContext.Provider value={modal}>
       <div className="app">
-        <h1 className="app__title">NASA API Browser</h1>
-        <SearchBar
-          input={input}
+        <div className="header">
+          <h1 className="app__title">NASA API Browser</h1>
+          <SearchBar
+            input={input}
+            setInput={setInput}
+            fetchNasaData={fetchNasaData}
+          />
+          <FocusModal />
+          <Links links={links} fetchNasaData={fetchNasaData} />
+        </div>
+
+        <Items
           setInput={setInput}
+          items={items}
           fetchNasaData={fetchNasaData}
         />
-        <FocusModal />
-
-        {loading ? (
-          <p className="app__loader">Chargement...</p>
-        ) : (
-          <>
-            <Links links={links} fetchNasaData={fetchNasaData} />
-            <Items
-              setInput={setInput}
-              items={items}
-              fetchNasaData={fetchNasaData}
-            />
-            <Links links={links} fetchNasaData={fetchNasaData} />
-          </>
-        )}
+        <Links links={links} fetchNasaData={fetchNasaData} />
+        {loading ? <p className="app__loader">Chargement...</p> : <></>}
       </div>
     </ModalContext.Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
